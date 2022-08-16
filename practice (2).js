e81171481d96649e2ceaca0928b1df4e6fefc799
ArvzRen <85274447+ArvzRen@users.noreply.github.com>
2022-08-16 16:18:35 -0500

    Add files via upload

---
+++ practice (2).js
@@ -0,0 +1,12 @@
+
+// AÑADE LOS ENLACES FIREBASE
+
+function addUser()
+{
+  user_name = document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+    purpose : "adding user"
+  });
+}
+
+
